51475682c4ecc520fa2a7d78878b1befb27bbb67
Seokyeong <purewater7174@gmail.com>
2022-05-04 18:40:01 +0900

    2주차 과제 gitignore 추가

---
+++ seminar2/level2/express-level2/src/api/user.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response, Router } from 'express';
+
+const router: Router = express.Router();
+
+router.get('/', (req: Request, res: Response) => {
+    return res.status(200).json({
+        status: 200,
+        message: '유저 조회 성공',
+        data: {
+            name: '황서경',
+        },
+    });
+});
+
+module.exports = router;
