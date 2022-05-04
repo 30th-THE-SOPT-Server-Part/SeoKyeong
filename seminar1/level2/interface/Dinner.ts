20c3000d4d04274ff068acab977d2f86c609def0
Seokyeong <purewater7174@gmail.com>
2022-05-04 18:37:37 +0900

    seminar2브랜치에 있는 seminar1 폴더 삭제

--- seminar1/level2/interface/Dinner.ts
+++
@@ -1,8 +0,0 @@
-import { Member } from './Member';
-
-// Dinner interface
-export interface Dinner {
-    member: Member[];
-    shuffle(array: Member[]): Member[];
-    organize(array: Member[]): void;
-}
