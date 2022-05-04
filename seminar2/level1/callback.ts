701bcadbb440efbf1139243bcc16f4d6d323b188
Seo-Kyeong Hwang <64405757+Seokyeong237@users.noreply.github.com>
2022-05-04 19:04:52 +0900

    Delete seminar2 directory

--- seminar2/level1/callback.ts
+++
@@ -1,7 +0,0 @@
-console.log('Hello Server');
-
-setTimeout((): void => {
-    console.log('Hello Server 2');
-}, 3000);
-
-console.log('Hello Server 3');
--- seminar2/level2/express-level2/src/api/like.ts
+++
@@ -1,9 +0,0 @@
-import express, { Request, Response, Router } from 'express';
-
-const router: Router = express.Router();
-
-router.get('/', (req: Request, res: Response) => {
-    return res.send('<h1> 좋아요 누르기 성공!!')
-});
-
-module.exports = router;
